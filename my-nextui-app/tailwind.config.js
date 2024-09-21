26d13a729b47c148629b40ef06bcf154c53f5cf5
Md Fahim Imran <fahimimran0088@gmail.com>
2024-09-21 13:23:44 +0000

    Update UI with new color scheme and fix font color issues

--- my-nextui-app/tailwind.config.js
+++ my-nextui-app/tailwind.config.js
@@ -9,7 +9,15 @@ module.exports = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'yale-blue': '#01377d',
+        'blue-green': '#009dd1',
+        'non-photo-blue': '#97e7f5',
+        'sgbus-green': '#7ed348',
+        'jade': '#26b170',
+      },
+    },
   },
   darkMode: "class",
   plugins: [nextui()],
